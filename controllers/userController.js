c78554b24870e2e7c86c3b918df34e6637eefb4a
iqrafareed <iqrafareed56@gmail.com>
2023-06-28 17:41:50 +0500

    login method

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,6 +39,33 @@ const registerUsers = asyncHandler(async (req, res) => {
 // @route POST api/users/login
 // @access public
 
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  const user = await User.findOne({ email });
+  // compare password with hashed password
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          userName: user.userName,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("Email or password is incorrect");
+  }
+});
+
 // @desc current user info
 // @route GET api/users/current
 // @access private
